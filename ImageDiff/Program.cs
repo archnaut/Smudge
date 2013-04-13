65729f76ae51360853a03fa423c89b0dadb8b713
Paul Clements <paulcclements@gmail.com>
2013-04-12 20:22:00 -0500

    initial

---
+++ ImageDiff/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace ImageDiff
+{
+	class Program
+	{
+	    public static void Main()
+	    {
+	    	Bitmap cleanTarget = new Bitmap("CleanTarget.PNG");
+	    	Bitmap dirtyTarget = new Bitmap("DirtyTarget.PNG");
+	    	
+	        Bitmap diff = ImageTool.GetDifferenceImage(cleanTarget, dirtyTarget, Color.Pink);
+	        diff.MakeTransparent(Color.Pink);
+	        diff.Save("test-diff.png",ImageFormat.Png);
+	    }
+	}
+}
